2aae1bc98dcb14c74a12369365cd18b912828296
Alex <volk2390@gmail.com>
2020-08-28 22:24:13 +0300

    Добавил новый фрагмент и в нем 2 поля для ввода текста (зоголовок и описание заментки)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.example.myapplication.ui.MainFragment
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initMainFragment()
+    }
+
+    private fun initMainFragment() {
+        var mainFragment: MainFragment = MainFragment()
+        var ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, mainFragment)
+        ft.commit()
     }
 }
---
+++ app/src/main/java/com/example/myapplication/ui/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.example.myapplication.R
+import com.squareup.picasso.Picasso
+
+class MainFragment : Fragment() {
+
+    lateinit var imageBackground: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view: View = inflater.inflate(R.layout.fragment_main, container, false)
+              return view
+    }
+
+
+}
